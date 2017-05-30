d6e2db27043f498fb9684a47a11cad8907829917
Matthieu <supermatou@matthieu.local>
2017-05-30 13:58:18 +0200

    Récupération de l'accout_id en insérant le pseudo du joueur

--- WorldOfTanks/ViewController.swift
+++ WorldOfTanks/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var ui_textPlayer: UITextField!
+    @IBOutlet weak var ui_labelAccount: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func _btn_validSearch(_ sender: Any) {
+        searchPlayer()
+    }
+    
+    
+    func searchPlayer () {
+        let pseudo: String = ui_textPlayer.text!
+        Alamofire.request("https://api.worldoftanks.eu/wot/account/list/?application_id=demo&search=\(pseudo)").responseJSON(completionHandler: { (response:DataResponse<Any>) in
+            var todoAsString = ""
+            debugPrint(response)
+            if let jsonArray = response.result.value as? [String:Any],
+                let data = jsonArray["data"] as? [Any],
+                let firstObject = data.first as? [String:Any] {
+                debugPrint(jsonArray)
+                
+                todoAsString += "Numéro de compte : \(firstObject["account_id"] as! Int) \n"
+                //                for todoData:Any in jsonArray {
+                //                    if let todoItem:[String:Any] = todoData as? [String:Any] {
+                //                        print(todoItem)
+                //                        todoAsString += "Numéro de compte : \(todoItem["account_id"] as! String) \n"
+                //                    }
+                //                }
+            }
+            self.ui_labelAccount.text = todoAsString
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
