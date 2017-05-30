9ce20176150899f63f2a98566ac67b0129b5326a
Matthieu <supermatou@matthieu.local>
2017-05-30 14:48:27 +0200

    Ajout du switch case

--- WorldOfTanks/ViewController.swift
+++ WorldOfTanks/ViewController.swift
@@ -32,24 +32,30 @@ class ViewController: UIViewController {
     func searchPlayer () {
         let pseudo: String = ui_textPlayer.text!
         Alamofire.request("https://api.worldoftanks.eu/wot/account/list/?application_id=demo&search=\(pseudo)").responseJSON(completionHandler: { (response:DataResponse<Any>) in
-            var todoAsString = ""
-            debugPrint(response)
-            if let jsonArray = response.result.value as? [String:Any],
-                let data = jsonArray["data"] as? [Any],
-                let firstObject = data.first as? [String:Any] {
-                debugPrint(jsonArray)
-                
-                todoAsString += "Numéro de compte : \(firstObject["account_id"] as! Int) \n"
-                //                for todoData:Any in jsonArray {
-                //                    if let todoItem:[String:Any] = todoData as? [String:Any] {
-                //                        print(todoItem)
-                //                        todoAsString += "Numéro de compte : \(todoItem["account_id"] as! String) \n"
-                //                    }
-                //                }
+            switch response.result {
+            case .success:
+                var todoAsString = ""
+                debugPrint(response)
+                if let jsonArray = response.result.value as? [String:Any],
+                    let data = jsonArray["data"] as? [Any],
+                    let firstObject = data.first as? [String:Any] {
+                    
+                    todoAsString += "Numéro de compte : \(firstObject["account_id"] as! Int) \n"
+                    //                for todoData:Any in jsonArray {
+                    //                    if let todoItem:[String:Any] = todoData as? [String:Any] {
+                    //                        print(todoItem)
+                    //                        todoAsString += "Numéro de compte : \(todoItem["account_id"] as! String) \n"
+                    //                    }
+                    //                }
+                }
+                self.ui_labelAccount.text = todoAsString
+            case .failure(let error):
+                print(error)
+                self.ui_labelAccount.text = "Joueur inconnue"
             }
-            self.ui_labelAccount.text = todoAsString
         })
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
