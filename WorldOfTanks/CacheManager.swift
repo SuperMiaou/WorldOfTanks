4ec0ab1b2c5451141a0c5c06354a44d75bd5e453
Matthieu <supermatou@matthieu.local>
2017-06-01 14:02:49 +0200

    GROS CHANGEMENT DE PROGRAMME

---
+++ WorldOfTanks/CacheManager.swift
@@ -0,0 +1,20 @@
+//
+//  CacheManager.swift
+//  WorldOfTanks
+//
+//  Created by Matthieu on 01/06/2017.
+//  Copyright © 2017 Matthieu. All rights reserved.
+//
+
+import Foundation
+
+class CacheManager {
+    
+    func setData(_ data:String, forUrl url:String) {
+        
+    }
+    
+    func getData(forUrl url:String) -> String? {
+        return nil
+    }
+}
